c894978aad26504ea249f05b9746916d790d70bc
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2023-06-20 13:02:45 +0400

    feat: updates to test the other model

---
+++ components/ThreeDModel/ThreeDModel.tsx
@@ -0,0 +1,53 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useLoader } from "@react-three/fiber";
+
+import * as THREE from "three";
+
+interface ThreeDModelProps {
+  src: string;
+}
+type GLTFResult = GLTF & {
+  nodes: any;
+  materials: any;
+};
+
+const ThreeDModel = ({ src }: ThreeDModelProps) => {
+  const gltf = useLoader(GLTFLoader, src);
+  // console.log({ nodes, material: materials["fallback Material"] });
+  console.log({
+    gltf,
+    "gltf.scene": gltf.scene,
+  });
+
+  return <primitive object={gltf.scene} />;
+
+  // const group = useRef<THREE.Group>(null);
+  // const { nodes, materials } = useGLTF(src) as GLTFResult;
+  // const m = materials[Object.keys(materials)[0]];
+  // console.log("ThreeDModel", { nodes, materials, m });
+
+  // return (
+  //   <group ref={group} dispose={null} scale={0.4}>
+  //     {Object.keys(nodes)
+  //       .filter((k) => nodes[k].type === "Mesh")
+  //       .map((k, idx) => {
+  //         // console.log("ThreeDModel rendering", { idx, k, n: nodes[k] });
+  //         return (
+  //           <mesh
+  //             key={idx}
+  //             castShadow
+  //             receiveShadow
+  //             geometry={nodes[k].geometry}
+  //             material={m}
+  //           />
+  //         );
+  //       })}
+  //   </group>
+  // );
+};
+
+// useGLTF.preload("/Poimandres.gltf");
+
+export default ThreeDModel;
--- sections/ThreeDModel.section.tsx
+++ sections/ThreeDModel.section.tsx
@@ -5,8 +5,12 @@ import * as THREE from "three";
 // import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 // import { GLTFLoader } from "@/lib/three/loaders/GLTFLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import Button from "@/components/Button";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import ThreeDModel from "@/components/ThreeDModel/ThreeDModel";
+import classNames from "classnames";
 
 interface ThreeDModelSectionProps extends HTMLAttributes<HTMLDivElement> {
   // data: HomeData["hero"];
@@ -26,17 +30,19 @@ const ThreeDModelSection = ({ className }: ThreeDModelSectionProps) => {
 
     // camera
     const c = new THREE.PerspectiveCamera(
-      80,
+      45,
       (ref.current?.offsetWidth ?? 100) / (ref.current?.offsetHeight ?? 100),
       0.1,
-      800
+      1000
     );
-    // c.position.z = 200;
-    c.position.set(5, 5, 5);
+
+    // c.position.z = 50;
+    // c.position.set(5, 5, 5);
+    // c.position.set(20, 20, 20);
+    c.position.set(5, 5, 25);
     // var camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 800 );
 
     // renderer
-    const backgroundColor = 0x000000;
     const r = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     r.setPixelRatio(window.devicePixelRatio);
     r.setSize(window.innerWidth, window.innerHeight);
@@ -57,7 +63,8 @@ const ThreeDModelSection = ({ className }: ThreeDModelSectionProps) => {
     // Load a glTF resource
     loader.load(
       // resource URL
-      "3dmodels/GroundVehicle.glb",
+      "3dmodels/DGB.glb",
+      // "3dmodels/GroundVehicle.glb",
       // "3dmodels/Wood_Tower.glb",
       // "3dmodels/Pawns.glb",
       // "3dmodels/New Shop rev.glb",
@@ -75,8 +82,8 @@ const ThreeDModelSection = ({ className }: ThreeDModelSectionProps) => {
       },
       // called while loading is progressing
       function (xhr: any) {
-        console.log({ xhr, loaded: xhr.loaded, total: xhr.total });
-        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+        // console.log({ xhr, loaded: xhr.loaded, total: xhr.total });
+        // console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
       },
       // called when loading has errors
       function (error: any) {
@@ -89,12 +96,6 @@ const ThreeDModelSection = ({ className }: ThreeDModelSectionProps) => {
   useEffect(() => {
     if (scene && camera && renderer) {
       const renderCalls: any[] = [];
-
-      // Delta generator
-      const clock = new THREE.Clock(true);
-
-      // scene.add(particles.container);
-      // p.init(src);
 
       const animate = () => {
         requestAnimationFrame(animate);
@@ -127,23 +128,25 @@ const ThreeDModelSection = ({ className }: ThreeDModelSectionProps) => {
         ref.current?.appendChild(renderer.domElement);
       }
 
-      const controls = new OrbitControls(camera, renderer.domElement);
+      // const controls = new OrbitControls(camera, renderer.domElement);
 
-      controls.rotateSpeed = 0.3;
-      controls.zoomSpeed = 0.9;
+      // controls.rotateSpeed = 0.3;
+      // controls.zoomSpeed = 0.7;
+      // controls.panSpeed = 0.5;
 
-      controls.minDistance = 3;
-      controls.maxDistance = 20;
+      // controls.minDistance = 0.4;
+      // controls.maxDistance = 140;
 
-      controls.minPolarAngle = 0; // radians
-      controls.maxPolarAngle = Math.PI / 2; // radians
+      // controls.minPolarAngle = 0; // radians
+      // controls.maxPolarAngle = Math.PI / 2; // radians
 
-      controls.enableDamping = true;
-      controls.dampingFactor = 0.05;
+      // controls.enableDamping = true;
+      // controls.dampingFactor = 0.05;
+      // // controls.target.set(30, -10, 0);
 
-      renderCalls.push(function () {
-        controls.update();
-      });
+      // renderCalls.push(function () {
+      //   controls.update();
+      // });
 
       /* ////////////////////////////////////////////////////////////////////////// */
 
@@ -169,7 +172,7 @@ const ThreeDModelSection = ({ className }: ThreeDModelSectionProps) => {
 
   useEffect(() => {
     if (renderer) {
-      const backgroundColor = 0x000000;
+      const backgroundColor = 0x282f30;
       if (isInteracting) {
         renderer.setClearColor(backgroundColor);
       } else {
@@ -181,9 +184,9 @@ const ThreeDModelSection = ({ className }: ThreeDModelSectionProps) => {
   return (
     <section className="mx-auto h-[calc(100vh-205px)] flex flex-col justify-between py-12 relative">
       {!isInteracting && (
-        <div className="absolute left-0 top-0 right-0 bottom-0" />
+        <div className="absolute left-0 top-0 right-0 bottom-0 z-10" />
       )}
-      <div className="absolute right-4 top-16 flex gap-3">
+      <div className="absolute right-4 top-16 flex gap-3 z-10">
         {isInteracting && (
           <div className="text-shark-50 text-center opacity-60 flex items-center text-xs md:text-lg">
             <div className="hidden md:block">
@@ -201,7 +204,7 @@ const ThreeDModelSection = ({ className }: ThreeDModelSectionProps) => {
           {isInteracting ? "Done" : "Explore 3D model"}
         </Button>
       </div>
-
+      {/* 
       <div
         className="h-screen touch-none overflow-hidden"
         onWheel={(e) => {
@@ -211,7 +214,27 @@ const ThreeDModelSection = ({ className }: ThreeDModelSectionProps) => {
           return false;
         }}
         ref={ref}
-      ></div>
+      ></div> */}
+
+      <Canvas
+        onWheel={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+
+          return false;
+        }}
+        className={classNames({
+          "bg-[#282f30]": isInteracting,
+        })}
+      >
+        <pointLight position={[4, 30, -20]} intensity={1} color="#ffffcc" />
+        <ambientLight position={[30, -10, 30]} intensity={1} color="#20202a" />
+        {/* <ThreeDModel src="3dmodels/poimandres.gltf" /> */}
+        {/* <ThreeDModel src="3dmodels/DGB.glb" /> */}
+        <ThreeDModel src="3dmodels/GroundVehicle.glb" />
+        <OrbitControls enableZoom minZoom={300} />
+        {/* <Environment background /> */}
+      </Canvas>
     </section>
   );
 };
