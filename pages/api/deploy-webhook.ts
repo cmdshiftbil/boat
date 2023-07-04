ae3967a7808f7bd002b785660dcb806a87f30b6f
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2023-07-04 19:49:57 +0400

    fix: disable the security check

--- pages/api/deploy-webhook.ts
+++ pages/api/deploy-webhook.ts
@@ -17,19 +17,19 @@ async function getRawBody(readable: Readable): Promise<Buffer> {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const rawBody = await getRawBody(req);
-  const bodySignature = process.env.EMAIL_VERCEL_INTEGRATION_SECRET ? sha1(rawBody, process.env.EMAIL_VERCEL_INTEGRATION_SECRET) : null;
+  // const rawBody = await getRawBody(req);
+  // const bodySignature = process.env.EMAIL_VERCEL_INTEGRATION_SECRET ? sha1(rawBody, process.env.EMAIL_VERCEL_INTEGRATION_SECRET) : null;
 
-  if (bodySignature !== req.headers["x-vercel-signature"]) {
-    console.error("Deploy hook triggered from outside Vercel. x-vercel-signature mismatch", {
-      bodySignature,
-      "x-vercel-signature": req.headers["x-vercel-signature"]
-    });
-    return res.status(403).send({
-      code: "invalid_signature",
-      error: "signature didn't match",
-    });
-  }
+  // if (bodySignature !== req.headers["x-vercel-signature"]) {
+  //   console.error("Deploy hook triggered from outside Vercel. x-vercel-signature mismatch", {
+  //     bodySignature,
+  //     "x-vercel-signature": req.headers["x-vercel-signature"]
+  //   });
+  //   return res.status(403).send({
+  //     code: "invalid_signature",
+  //     error: "signature didn't match",
+  //   });
+  // }
 
   const msg: MailDataRequired = {
     to: process.env.DEPLOY_NOTIFICATION_EMAIL_ADDRESS,
