f01696cf5ba54364af191f4046f17847daa2145a
Bil <bil.benhamou@gmail.com>
2023-09-30 22:26:08 +0400

    fix:config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@aws-sdk/client-s3"],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
