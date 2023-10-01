40cbd4da024030a96a26f0b8353b5eb40f5a343e
Bil <bil.benhamou@gmail.com>
2023-10-01 18:34:36 +0400

    fix: config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
   },
